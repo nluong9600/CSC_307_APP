88672d83f52452ca0dadba0cb5abf549ff30de55
nluong9600 <nluong9600@gmail.com>
2025-01-29 16:00:32 -0800

    Successfull added POST request ability

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -33,13 +33,30 @@ function MyApp() {
   }
 
   function updateList(person) {
-    setCharacters([...characters, person])
+    //old function used for adding characters?: setCharacters([...characters, person])
+    postUser(person)
+    .then(() => setCharacters([...characters, person]))
+    .catch((error) => {
+      console.log(error);
+    });
   }
 
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
     return promise;
   } //Promises are the basis of asynchronous processing in Javascript
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(person)
+    });
+  
+    return promise;
+  }
 
   useEffect(() => {
     fetchUsers()
